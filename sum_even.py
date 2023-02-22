889d695bc5a544883d00edf12c758621326d5911
stranik87 <tulkin.boynazarov@gmail.com>
2023-02-22 06:55:58 +0300

    sumEvent

--- sum_even.py
+++ sum_even.py
@@ -1,6 +1,13 @@
 #A four-digit integer is given. Find the sum of even digits in it.
 #Create a variable "var_int" and assign it a four-digit integer value.
-
+var_int = 1234
 #Create a variable "sum_even" and assign it 0.
 
 #Find the sum of the even digits in the variable "var_int".
+x1 = var_int%10
+x2 = var_int//10%10
+x3 = var_int //100%10
+x4 = var_int//1000%10
+
+
+print(((x4 + 1 )%2 * x4) + ((x3+1)%2 * x3) + ((x2 + 1)%2 * x2) + ((x1+1)%2 * x1))
