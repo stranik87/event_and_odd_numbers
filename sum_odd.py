4e6c1d8497fa39478cc319a42b2c5b9322338138
stranik87 <tulkin.boynazarov@gmail.com>
2023-02-22 07:13:30 +0300

    sumOdd

--- sum_odd.py
+++ sum_odd.py
@@ -1,6 +1,15 @@
 #A four-digit integer is given. Find the sum of odd digits in it.
 #Create a variable "var_int" and assign it a four-digit integer value.
-
+var_int = 4567
 #Create a variable "sum_odd" and assign it 0.
-
+sum_odd = 0
 #Find the sum of the odd digits in the variable "var_int".
+x1 = var_int%10
+x2 = var_int//10%10
+x3 = var_int //100%10
+x4 = var_int//1000%10
+
+
+sum_odd = ((x4%2)*x4) + ((x3%2)*x3) + ((x2%2)*x2) + ((x1%2)*x1)
+
+print(sum_odd)
