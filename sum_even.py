eff216d97c9bcb27fdbc4b4761dfb7ada8956cac
stranik87 <tulkin.boynazarov@gmail.com>
2023-02-22 07:01:33 +0300

    sumEvent

--- sum_even.py
+++ sum_even.py
@@ -8,6 +8,6 @@ x1 = var_int%10
 x2 = var_int//10%10
 x3 = var_int //100%10
 x4 = var_int//1000%10
+sum_even = ((x4 + 1 )%2 * x4) + ((x3+1)%2 * x3) + ((x2 + 1)%2 * x2) + ((x1+1)%2 * x1)
 
-
-print(((x4 + 1 )%2 * x4) + ((x3+1)%2 * x3) + ((x2 + 1)%2 * x2) + ((x1+1)%2 * x1))
+print(sum_even)
